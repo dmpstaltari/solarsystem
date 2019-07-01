f32f142228f73c689218a83c0b5fbd7bc470587a
Dario Perez Staltari <dmpstaltari@gmail.com>
2019-07-01 17:44:10 -0300

    the way of recovering the default configuration file is modified

--- src/main/java/com/ml/solarsystem/config/SolarSystemDefaultConfig.java
+++ src/main/java/com/ml/solarsystem/config/SolarSystemDefaultConfig.java
@@ -1,13 +1,12 @@
 package com.ml.solarsystem.config;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ml.solarsystem.dtos.SolarSystemDTO;
@@ -21,15 +20,23 @@ public class SolarSystemDefaultConfig {
 
 	static {
 		ObjectMapper objectMapper = new ObjectMapper();
-		InputStream input;
+		InputStream input = null;
 		try {
-			File file = ResourceUtils.getFile("classpath:solar-system-default-config.json");
-			input = new FileInputStream(file);
+			Resource resource = new ClassPathResource("solar-system-default-config.json");
+			input = resource.getInputStream();
 			SolarSystemDTO solarSystemDTO = objectMapper.readValue(input, SolarSystemDTO.class);
 			instance = new SolarSystemDefaultConfig();
 			instance.solarSystemDto = solarSystemDTO;
 		} catch (IOException e) {
 			log.error("Default configuration could not be loaded: {}", e.getMessage());
+		}finally{
+			if(input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					log.error("Fail to close input stream: {}", e.getMessage());
+				}
+			}
 		}
 	}
 	
